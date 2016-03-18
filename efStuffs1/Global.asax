dc1f546077afe4dd6c3a5da2d1985855ba1537b5
Waseem Soomro <ws@waseemsoomro.com>
2016-03-18 16:06:31 +0100

    efStuffs1
    playing around with EF6

---
+++ efStuffs1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="efStuffs1.MvcApplication" Language="C#" %>
---
+++ efStuffs1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(efStuffs1.Startup))]
+namespace efStuffs1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
